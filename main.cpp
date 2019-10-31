b91f678ff23acfe7258341d697b4b97491ae0b32
rkdms0327 <55428479+rkdms0327@users.noreply.github.com>
2019-10-31 16:08:57 +0900

    06

--- main.cpp
+++ main.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <stdlib.h>
+#define ROWS 3
+#define COLS 3
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+{
+	int i, j;
+	for (i=0;i<ROWS;i++)
+		for(j=0;j<COLS;j++)
+			C[i][j]=A[i][j]+B[i][j];
+}
+
+void printMatrix(int A[][COLS])
+{
+	int i, j;
+	for (i=0;i<ROWS;i++){
+		for (j=0;j<COLS;j++)
+			printf("%i ",A[i][j]);
+		printf("\n");
+	}	
+}
 
 int main(int argc, char** argv) {
 	
-	int data[5];
-	int i;
+	int A[ROWS][COLS]={
+	{2,3,0},
+	{8,9,1},
+	{7,0,5}
+	};
 	
-	data[0]=10;
-	data[1]=20;
-	data[2]=30;
-	data[3]=40;
-	data[4]=50;
+	int B[ROWS][COLS]={
+	{1,0,0},
+	{0,1,0},
+	{0,0,1}
+	};
 	
-	for (i=0; i<5; i++)
-		printf("data[%i]=%i\n",i, data[i]);
-		
+	int C[ROWS][COLS];
+	
+	
+	addMatrix(A,B,C);
+	printMatrix(C);
+	
+
 	return 0;
 }
